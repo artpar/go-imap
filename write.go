afaf644dbf50524bbddafd954870eccad9c7931e
Parth Mudgal <artpar@gmail.com>
2020-02-23 12:17:26 +0530

    use quoted for writing string in body

--- write.go
+++ write.go
@@ -199,7 +199,7 @@ func (w *Writer) writeField(field interface{}) error {
 		return w.writeString(field.String())
 	case *BodySectionName:
 		// Can contain spaces - that's why we don't just pass it as a string
-		return w.writeString(string(field.FetchItem()))
+		return w.writeString(Quoted(field.FetchItem()))
 	}
 
 	return fmt.Errorf("imap: cannot format field: %v", field)
